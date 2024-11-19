fa78fb7e32a844cf1596b88b29043bb775222bd6
LiHui_QIAN <1157577993@qq.com>
2024-11-19 17:56:24 +0800

    完成lab3

--- libsponge/tcp_sender.cc
+++ libsponge/tcp_sender.cc
@@ -3,6 +3,7 @@
 #include "tcp_config.hh"
 
 #include <random>
+#include <iostream>
 
 // Dummy implementation of a TCP sender
 
@@ -17,22 +18,186 @@ using namespace std;
 //! \param[in] capacity the capacity of the outgoing byte stream
 //! \param[in] retx_timeout the initial amount of time to wait before retransmitting the oldest outstanding segment
 //! \param[in] fixed_isn the Initial Sequence Number to use, if set (otherwise uses a random ISN)
+// TCPSender::TCPSender(const size_t capacity, const uint16_t retx_timeout, const std::optional<WrappingInt32> fixed_isn)
+// 	: _isn(fixed_isn.value_or(WrappingInt32{ random_device()() }))
+// 	, _initial_retransmission_timeout{ retx_timeout }
+// 	, _stream(capacity) {
+// }
 TCPSender::TCPSender(const size_t capacity, const uint16_t retx_timeout, const std::optional<WrappingInt32> fixed_isn)
-    : _isn(fixed_isn.value_or(WrappingInt32{random_device()()}))
-    , _initial_retransmission_timeout{retx_timeout}
-    , _stream(capacity) {}
+	: _isn(fixed_isn.value_or(WrappingInt32{ random_device()() }))
+	, _initial_retransmission_timeout{ retx_timeout }
+	, _stream(capacity)
+	, _rto{ retx_timeout } {
+}
 
-uint64_t TCPSender::bytes_in_flight() const { return {}; }
 
-void TCPSender::fill_window() {}
+uint64_t TCPSender::bytes_in_flight() const {
+	return _bytes_in_flight;
+}
+
+
+void TCPSender::fill_window() {
+	if (!_syn_sent) {
+		_syn_sent = true;
+		TCPSegment seg;
+		seg.header().syn = true;
+		_send_segment(seg);
+		return;
+	}
+
+	// 如果携带syn的segment都还没被确认，就什么都不做
+	if (!_segments_outstanding.empty() && _segments_outstanding.front().header().syn)
+		return;
+
+	// If _stream is empty but input has not ended, do nothing.
+	if (!_stream.buffer_size() && !_stream.eof())
+		// Lab4 behavior: if incoming_seg.length_in_sequence_space() is not zero, send ack.
+		return;
+
+	if (_fin_sent)
+		return;
+
+	if (_receiver_window_size) {
+		while (_receiver_free_space) {
+			TCPSegment seg;
+			size_t payload_size = min({ _stream.buffer_size(),
+									   static_cast<size_t>(_receiver_free_space),
+									   static_cast<size_t>(TCPConfig::MAX_PAYLOAD_SIZE) });
+
+			// 要发送的数据存在_stream中的
+			seg.payload() = Buffer{ _stream.read(payload_size) };
+
+			// 如果payload_size大于receiver的接受空间，说明至少还需要两次发送，所以还不能fin
+			if (_stream.eof() && static_cast<size_t>(_receiver_free_space) > payload_size) {
+				seg.header().fin = true;
+				_fin_sent = true;
+			}
+
+			_send_segment(seg);
+
+			if (_stream.buffer_empty())
+				break;
+		}
+	}
+	else if (_receiver_free_space == 0) {
+		// 如果远程窗口大小为 0, 则把其视为 1 进行操作(为了让sender能收到ack反馈)
+		TCPSegment seg;
+
+		if (_stream.eof()) {
+			seg.header().fin = true;
+			_fin_sent = true;
+			_send_segment(seg);
+		}
+
+		else if (!_stream.buffer_empty()) {
+			seg.payload() = Buffer{ _stream.read(1) };
+			_send_segment(seg);
+		}
+	}
+}
 
 //! \param ackno The remote receiver's ackno (acknowledgment number)
 //! \param window_size The remote receiver's advertised window size
-void TCPSender::ack_received(const WrappingInt32 ackno, const uint16_t window_size) { DUMMY_CODE(ackno, window_size); }
+void TCPSender::ack_received(const WrappingInt32 ackno, const uint16_t window_size) {
+	// pop seg from segments_outstanding
+	// deduct bytes_inflight
+	// reset rto, reset _consecutive_retransmissions
+	// reset timer
+	// stop timer if bytes_inflight == 0
+	// fill window
+	// update _receiver_window_size
+	uint64_t abs_ackno = unwrap(ackno, _isn, _next_seqno);
+	if (!_ack_valid(abs_ackno)) {
+		cout << "invalid ackno!\n";
+		return;
+	}
+
+	_receiver_window_size = window_size;
+	_receiver_free_space = window_size;
+
+	while (!_segments_outstanding.empty()) {
+		TCPSegment seg = _segments_outstanding.front();
+		// 如果队头的seg收到了ack，则出队
+		if (unwrap(seg.header().seqno, _isn, _next_seqno) + seg.length_in_sequence_space() <= abs_ackno) {
+			_bytes_in_flight -= seg.length_in_sequence_space();
+			_segments_outstanding.pop();
+			_time_elapsed = 0;
+			_rto = _initial_retransmission_timeout;
+			_consecutive_retransmissions = 0;
+		}
+		else
+			break;
+	}
+
+	if (!_segments_outstanding.empty()) {
+		_receiver_free_space = static_cast<uint16_t>(abs_ackno + static_cast<uint64_t>(window_size) -
+			unwrap(_segments_outstanding.front().header().seqno, _isn, _next_seqno) - _bytes_in_flight);
+	}
+
+	// 还没开始发送segment时就不启动计时器了
+	if (!_bytes_in_flight)
+		_timer_running = false;
+	fill_window();
+}
+
 
 //! \param[in] ms_since_last_tick the number of milliseconds since the last call to this method
-void TCPSender::tick(const size_t ms_since_last_tick) { DUMMY_CODE(ms_since_last_tick); }
+void TCPSender::tick(const size_t ms_since_last_tick) {
+	if (!_timer_running)
+		return;
 
-unsigned int TCPSender::consecutive_retransmissions() const { return {}; }
+	_time_elapsed += ms_since_last_tick;
+	if (_time_elapsed >= _rto) {
+		_segments_out.push(_segments_outstanding.front());
+		if (_receiver_window_size || _segments_outstanding.front().header().syn) {
+			// 每次重传把时间阈值×2（避免网络拥塞时还在不断发送），且记录重传次数
+			_consecutive_retransmissions++;
+			_rto *= 2;
+		}
+		_time_elapsed = 0;  // 记得清零记录时间
+	}
+}
 
-void TCPSender::send_empty_segment() {}
+
+unsigned int TCPSender::consecutive_retransmissions() const {
+	return _consecutive_retransmissions;
+}
+
+
+void TCPSender::send_empty_segment() {
+	TCPSegment seg;
+	seg.header().seqno = wrap(_next_seqno, _isn);
+	_segments_out.push(seg);
+}
+
+
+bool TCPSender::_ack_valid(uint64_t abs_ackno) {
+	// 如果传入的ack不可靠则直接丢弃(都还没发送，怎么可能接到确认号？)
+	if (_segments_outstanding.empty())
+		return abs_ackno <= _next_seqno;
+	return (abs_ackno <= _next_seqno &&
+		abs_ackno >= unwrap(_segments_outstanding.front().header().seqno, _isn, _next_seqno));
+}
+
+
+void TCPSender::_send_segment(TCPSegment& seg) {
+	seg.header().seqno = wrap(_next_seqno, _isn);
+	_next_seqno += seg.length_in_sequence_space();
+	_bytes_in_flight += seg.length_in_sequence_space();
+
+	if (_syn_sent)
+		_receiver_free_space -= seg.length_in_sequence_space();
+
+	_segments_out.push(seg);
+	_segments_outstanding.push(seg);
+
+	// 一旦开始发送segment就要启动计时器
+	if (!_timer_running) {
+		_timer_running = true;
+		_time_elapsed = 0;
+	}
+	// cout << "seqno: " << seg.header().seqno;
+	// cout << "payload " << seg.payload().str();
+	// cout << " receiver_free_space " << _receiver_free_space;
+	// cout << " seg_length " << seg.length_in_sequence_space() << "\n";
+}
--- libsponge/tcp_sender.hh
+++ libsponge/tcp_sender.hh
@@ -8,6 +8,7 @@
 
 #include <functional>
 #include <queue>
+#include <map>
 
 //! \brief The "sender" part of a TCP implementation.
 
@@ -16,77 +17,113 @@
 //! maintains the Retransmission Timer, and retransmits in-flight
 //! segments if the retransmission timer expires.
 class TCPSender {
-  private:
-    //! our initial sequence number, the number for our SYN.
-    WrappingInt32 _isn;
+private:
+	//! our initial sequence number, the number for our SYN.
+	WrappingInt32 _isn;
 
-    //! outbound queue of segments that the TCPSender wants sent
-    std::queue<TCPSegment> _segments_out{};
+	//! outbound queue of segments that the TCPSender wants sent
+	std::queue<TCPSegment> _segments_out{};
 
-    //! retransmission timer for the connection
-    unsigned int _initial_retransmission_timeout;
+	//! retransmission timer for the connection
+	unsigned int _initial_retransmission_timeout;
 
-    //! outgoing stream of bytes that have not yet been sent
-    ByteStream _stream;
+	//! outgoing stream of bytes that have not yet been sent
+	ByteStream _stream;
 
-    //! the (absolute) sequence number for the next byte to be sent
-    uint64_t _next_seqno{0};
+	//! the (absolute) sequence number for the next byte to be sent
+	uint64_t _next_seqno{ 0 };
 
-  public:
-    //! Initialize a TCPSender
-    TCPSender(const size_t capacity = TCPConfig::DEFAULT_CAPACITY,
-              const uint16_t retx_timeout = TCPConfig::TIMEOUT_DFLT,
-              const std::optional<WrappingInt32> fixed_isn = {});
 
-    //! \name "Input" interface for the writer
-    //!@{
-    ByteStream &stream_in() { return _stream; }
-    const ByteStream &stream_in() const { return _stream; }
-    //!@}
+	/***********************添加的一些私有变量****************************/
+	// size_t last_window_size{ 1 };
+	// size_t outgoing_bytes{ 0 };
+	// bool set_syn_flag{ false };
+	// bool set_fin_flag{ false };
 
-    //! \name Methods that can cause the TCPSender to send a segment
-    //!@{
+	// 
 
-    //! \brief A new acknowledgment was received
-    void ack_received(const WrappingInt32 ackno, const uint16_t window_size);
+	// std::map<size_t, TCPSegment> outgoing_map{};
 
-    //! \brief Generate an empty-payload segment (useful for creating empty ACK segments)
-    void send_empty_segment();
+	// int timeout{ -1 };  // 设定的超时时间
+	// int timecount{ 0 };  // 累计时间
+	// size_t consecutive_retransmissions_count{ 0 };
 
-    //! \brief create and send segments to fill as much of the window as possible
-    void fill_window();
+	bool _syn_sent = false;
+	bool _fin_sent = false;
 
-    //! \brief Notifies the TCPSender of the passage of time
-    void tick(const size_t ms_since_last_tick);
-    //!@}
+	uint64_t _bytes_in_flight = 0;
+	uint16_t _receiver_window_size = 0;
+	uint16_t _receiver_free_space = 0;  // 接收方的剩余可用空间
+	uint16_t _consecutive_retransmissions = 0;
 
-    //! \name Accessors
-    //!@{
+	unsigned int _rto = 0;  // 设置的重传时间阈值
+	unsigned int _time_elapsed = 0;
+	bool _timer_running = false;
+	std::queue<TCPSegment> _segments_outstanding{};  // 已发送但未被接收的segment;
 
-    //! \brief How many sequence numbers are occupied by segments sent but not yet acknowledged?
-    //! \note count is in "sequence space," i.e. SYN and FIN each count for one byte
-    //! (see TCPSegment::length_in_sequence_space())
-    size_t bytes_in_flight() const;
+	// Lab4 modify:
+	// bool _fill_window_called_by_ack_received{false};
 
-    //! \brief Number of consecutive retransmissions that have occurred in a row
-    unsigned int consecutive_retransmissions() const;
+	bool _ack_valid(uint64_t abs_ackno);
+	void _send_segment(TCPSegment& seg);
+	/*******************************************************************/
 
-    //! \brief TCPSegments that the TCPSender has enqueued for transmission.
-    //! \note These must be dequeued and sent by the TCPConnection,
-    //! which will need to fill in the fields that are set by the TCPReceiver
-    //! (ackno and window size) before sending.
-    std::queue<TCPSegment> &segments_out() { return _segments_out; }
-    //!@}
 
-    //! \name What is the next sequence number? (used for testing)
-    //!@{
+public:
+	//! Initialize a TCPSender
+	TCPSender(const size_t capacity = TCPConfig::DEFAULT_CAPACITY,
+		const uint16_t retx_timeout = TCPConfig::TIMEOUT_DFLT,
+		const std::optional<WrappingInt32> fixed_isn = {});
 
-    //! \brief absolute seqno for the next byte to be sent
-    uint64_t next_seqno_absolute() const { return _next_seqno; }
+	//! \name "Input" interface for the writer
+	//!@{
+	ByteStream& stream_in() { return _stream; }
+	const ByteStream& stream_in() const { return _stream; }
+	//!@}
 
-    //! \brief relative seqno for the next byte to be sent
-    WrappingInt32 next_seqno() const { return wrap(_next_seqno, _isn); }
-    //!@}
+	//! \name Methods that can cause the TCPSender to send a segment
+	//!@{
+
+	//! \brief A new acknowledgment was received
+	void ack_received(const WrappingInt32 ackno, const uint16_t window_size);
+
+	//! \brief Generate an empty-payload segment (useful for creating empty ACK segments)
+	void send_empty_segment();
+
+	//! \brief create and send segments to fill as much of the window as possible
+	void fill_window();
+
+	//! \brief Notifies the TCPSender of the passage of time
+	void tick(const size_t ms_since_last_tick);
+	//!@}
+
+	//! \name Accessors
+	//!@{
+
+	//! \brief How many sequence numbers are occupied by segments sent but not yet acknowledged?
+	//! \note count is in "sequence space," i.e. SYN and FIN each count for one byte
+	//! (see TCPSegment::length_in_sequence_space())
+	size_t bytes_in_flight() const;
+
+	//! \brief Number of consecutive retransmissions that have occurred in a row
+	unsigned int consecutive_retransmissions() const;
+
+	//! \brief TCPSegments that the TCPSender has enqueued for transmission.
+	//! \note These must be dequeued and sent by the TCPConnection,
+	//! which will need to fill in the fields that are set by the TCPReceiver
+	//! (ackno and window size) before sending.
+	std::queue<TCPSegment>& segments_out() { return _segments_out; }
+	//!@}
+
+	//! \name What is the next sequence number? (used for testing)
+	//!@{
+
+	//! \brief absolute seqno for the next byte to be sent
+	uint64_t next_seqno_absolute() const { return _next_seqno; }
+
+	//! \brief relative seqno for the next byte to be sent
+	WrappingInt32 next_seqno() const { return wrap(_next_seqno, _isn); }
+	//!@}
 };
 
 #endif  // SPONGE_LIBSPONGE_TCP_SENDER_HH
